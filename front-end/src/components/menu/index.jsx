f0ecd9dcb49d6ec8dc2fc9a12b72b3d78bc45b2f
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-06-17 23:23:19 -0300

    cadastrar livro font e back-end

--- front-end/src/components/menu/index.jsx
+++ front-end/src/components/menu/index.jsx
@@ -4,7 +4,7 @@ import { MenuItem } from "../MenuItem";
 export const Menu = () => {
     return(
         <nav className='navbar'>
-          <MenuItem text={'Cadastrar Filme'} />
+          <MenuItem text={'Cadastrar Filme'} linkl={'/cadastrarFilme/'}/>
           <MenuItem text={'Listar Filme'} linkl={'/listarFilme/'}/>
         </nav>
     );
--- front-end/src/pages/cadastrarFilmes/index.jsx
+++ front-end/src/pages/cadastrarFilmes/index.jsx
@@ -1,5 +1,74 @@
+import { useState } from 'react';
+
 export const CadastrarFilmes = () => {
+    const [filme, setFilme] = useState({
+        titulo: '',
+        descricao: '',
+        ano: '',
+      });
+
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFilme((prevFilme) => ({
+          ...prevFilme,
+          [name]: value,
+        }));
+      };
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+      
+        fetch('http://localhost:8000/api/filmes', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(filme),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data); // Exibir mensagem de sucesso
+            // Limpar o formulário
+            setFilme({
+              titulo: '',
+              descricao: '',
+              ano: '',
+            });
+          })
+          .catch((error) => {
+            console.error(error); // Exibir mensagem de erro
+          });
+      };
+
     return(
-        <h1>CadastrarFilmes</h1>
+        <form onSubmit={handleSubmit}>
+        <div>
+          <label>Título:</label>
+          <input
+            type="text"
+            name="titulo"
+            value={filme.titulo}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Descrição:</label>
+          <textarea
+            name="descricao"
+            value={filme.descricao}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div>
+          <label>Ano:</label>
+          <input
+            type="number"
+            name="ano"
+            value={filme.ano}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
     );
 };
