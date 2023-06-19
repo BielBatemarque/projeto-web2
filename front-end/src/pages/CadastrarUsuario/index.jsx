6e8ae0f467401ab77447fa89f64c1b7492028a9d
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-06-19 01:48:05 -0300

    telad e cadastro de usuários

--- front-end/src/pages/CadastrarUsuario/index.jsx
+++ front-end/src/pages/CadastrarUsuario/index.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import './CadastrarUsuario.css';
+import { useNavigate } from 'react-router-dom';
 
 export const CadastrarUsuario = () => {
+    const navigate = useNavigate();
     const [novoUser, setNovoUser] = useState({
         nome:'',
         email:'',
@@ -16,11 +18,41 @@ export const CadastrarUsuario = () => {
         }));
       };
 
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+      
+        try {
+          const response = await fetch('http://localhost:8000/api/usuarios', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(novoUser),
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data); 
+            setNovoUser({
+              nome: '',
+              email: '',
+              senha: '',
+            });
+            navigate('/');
+          } else {
+            throw new Error('Erro ao cadastrar usuário');
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+
+     
       console.log(novoUser);
 
 
     return(
-      <div className='form'>
+      <div className='form' onSubmit={handleSubmit}>
         <h1>Cadastrar Novo Usuário</h1>
         <form action="" className='formCadastrar'>
             <input type="text" name='nome' placeholder='Nome de usuários' onChange={handleChange}/>
