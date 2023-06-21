8dd34c2b1a729a8109b50f0bd2f17597a277b2ee
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-06-21 02:19:07 -0300

    homePage

--- front-end/src/components/menu/index.jsx
+++ front-end/src/components/menu/index.jsx
@@ -4,6 +4,7 @@ import { MenuItem } from "../MenuItem";
 export const Menu = () => {
     return(
         <nav className='navbar'>
+            <MenuItem text={'Home'} linkl={'/HomePage/'} />
             <MenuItem text={'Filmes'} linkl={'/listarFilmes/'}/>
             <MenuItem text={'Avaliações'} linkl={'/listarAvaliacoes/'}/>
         </nav>
--- front-end/src/pages/HomePage/index.jsx
+++ front-end/src/pages/HomePage/index.jsx
@@ -1,7 +1,23 @@
 import { Menu } from "../../components/menu";
+import './styles.css';
+import cine from '../../assets/cinema.jpg';
 
 export const HomePage = () => {
     return(
-        <Menu />
+        <div className="HomePage">
+            <Menu />
+                <h1>FilmeFlix</h1>
+            <div className="homeContent">
+                <div className="dv1">
+                    <h3>App de Avaliação de Filmes</h3>
+                    <div className="txt">
+                        <p>Com FilmeFLix você pode adicionar Avaliação a filmes do momento, deixando sua nota e suas consideração a respeito do filme</p>
+                    </div>
+                </div>
+                <div className="dv1"> 
+                     <img src={cine} alt="imagem cinema"  className="cinema"/>
+                </div>
+            </div>
+        </div>
     );
 }
