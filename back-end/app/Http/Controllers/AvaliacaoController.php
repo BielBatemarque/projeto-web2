b72e3e963c0a749f9a336e459b12da9d10caed99
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-03 01:48:39 -0300

    editar avaliacoes

--- back-end/app/Http/Controllers/AvaliacaoController.php
+++ back-end/app/Http/Controllers/AvaliacaoController.php
@@ -28,4 +28,32 @@ class AvaliacaoController extends Controller{
 
         return response()->json('Avaliação cadastrada com sucesso!');
     }
+
+    public function editarAvaliacoes(Request $request, $id){
+        // Primeiro, vamos validar os campos da requisição como no método de cadastro
+        $request->validate([
+            'filme_id' => 'required',
+            'user_id' => 'required',
+            'nota' => 'required|integer',
+            'descricao' => 'nullable|string',
+        ]);
+
+        // Localize a avaliação a ser atualizada pelo seu ID
+        $avaliacao = Avaliacao::find($id);
+
+        // Verifique se a avaliação foi encontrada
+        if (!$avaliacao) {
+            return response()->json('Avaliação não encontrada!', 404);
+        }
+
+        // Atualize os campos da avaliação com os valores da requisição
+        $avaliacao->filme_id = $request->input('filme_id');
+        $avaliacao->user_id = $request->input('user_id');
+        $avaliacao->nota = $request->input('nota');
+        $avaliacao->descricao = $request->input('descricao');
+        $avaliacao->save();
+
+        return response()->json('Avaliação atualizada com sucesso!');
+
+    }
 }
--- front-end/src/pages/EditarAvaliacao/index.jsx
+++ front-end/src/pages/EditarAvaliacao/index.jsx
@@ -1,18 +1,66 @@
 import { useParams } from 'react-router-dom';
 import './EditarAvaliacao.css';
+import { useState, useEffect } from 'react';
 
 export const EditarAvaliacao = () => {
-    const { id } = useParams();
-    console.log(id);
-    return(
-        <div className="editarAvaliacao">
-            <form >
+  const { id } = useParams();
+  console.log(id);
+
+  const [avaliacao, setAvaliacao] = useState({
+    filme_id: '',
+    user_id: '',
+    nota: '',
+    descricao: '',
+  });
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/api/avaliacoes/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setAvaliacao(data);
+      })
+      .catch((error) => {
+        console.error('Erro ao obter a avaliação:', error);
+      });
+  }, [id]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setAvaliacao({
+      ...avaliacao,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:8000/api/avaliacoes/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(avaliacao),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Avaliação atualizada com sucesso!', data);
+      })
+      .catch((error) => {
+        console.error('Erro ao atualizar a avaliação:', error);
+      });
+  };
+
+  return (
+    <div className="editarAvaliacao">
+      <form onSubmit={handleSubmit}>
         <div>
           <label>
             Filme ID:
             <input
               type="text"
               name="filme_id"
+              value={avaliacao.filme_id}
+              onChange={handleChange}
             />
           </label>
         </div>
@@ -22,6 +70,8 @@ export const EditarAvaliacao = () => {
             <input
               type="text"
               name="user_id"
+              value={avaliacao.user_id}
+              onChange={handleChange}
             />
           </label>
         </div>
@@ -31,6 +81,8 @@ export const EditarAvaliacao = () => {
             <input
               type="number"
               name="nota"
+              value={avaliacao.nota}
+              onChange={handleChange}
             />
           </label>
         </div>
@@ -39,11 +91,13 @@ export const EditarAvaliacao = () => {
             Descrição:
             <textarea
               name="descricao"
+              value={avaliacao.descricao}
+              onChange={handleChange}
             ></textarea>
           </label>
         </div>
-        <button type="submit">Avaliar</button>
+        <button type="submit">Atualizar Avaliação</button>
       </form>
-        </div>
-    );
-}
+    </div>
+  );
+};
